7db9c0440e4715d67c0d2ce9140d3554ec7a81ef
none <none@none.com>
2022-01-06 18:45:48 +0800

    ...

--- src/main/scala/com/cat/zsy/Application.scala
+++ src/main/scala/com/cat/zsy/Application.scala
@@ -7,6 +7,15 @@ import com.cat.zsy.util.StockUtils
 import com.cat.zsy.util.StockUtils.intToDecimal
 import org.slf4j.LoggerFactory
 
+/**
+ * 关注
+ * 300718;300461;300200;002050;002043;000063;300094;000888;002851;000938;000712;000927;002207；
+ * 601919;600988;688981;
+ *
+ * 待定
+ * 000878;002214;300117;300369;300719;600739;603186
+ * 其中:300369;002214;
+ */
 object Application extends App {
   private val log = LoggerFactory.getLogger(this.getClass)
 
@@ -14,36 +23,41 @@ object Application extends App {
 
   val begin = System.currentTimeMillis()
 
-  val codes =
-    "\"sh600178\";\"sh600258\";\"sh600706\";\"sh600845\";\"sh600960\";\"sh603112\";\"sh603607\";\"sh603969\";\"sh605255\";\"sh688196\";\"sz000010\";\"sz000063\";\"sz000530\";\"sz000712\";\"sz000888\";\"sz000927\";\"sz000938\";\"sz002043\";\"sz002050\";\"sz002207\";\"sz002236\";\"sz002278\";\"sz002370\";\"sz002703\";\"sz002831\";\"sz002851\";\"sz300021\";\"sz300094\";\"sz300125\";\"sz300200\";\"sz300229\";\"sz300461\";\"sz300481\";\"sz300626\";\"sz300680\";\"sz300718\";\"sz300806\";"
+  val codes = choose()
 
-//  val codes = "600258;600706;600845;600960;603112;603605;603607;603969;605255;688196";
+//  val codes = "300369;300117;002214;000878;300719;600739;603186;600988;"
+//    .split(";")
+//    .distinct
+//    .sorted
+//    .toList
 
-//  val codes = "000938;300200;002207;002851;300718;600845;002851;603357;002788"
-  show(codes.split(";").map(_.substring(3, 9)).toList)
-//  choose()
+  log.info(codes.mkString(";"))
+
+  show(codes, detail = false)
 
   val end = System.currentTimeMillis()
 
   log.warn("cost " + (end - begin) + " mills")
 
-  private def choose(): Unit = {
+  private def choose(): Seq[String] = {
     val list = TongStockApi.listAllDataFromFilepath(dir)
 
     val r = list
       .map(o => OscillationCalculator.calcList(o, List(100, 200, 300, 400)))
       .filter(_.indicators.nonEmpty) // 已退市的垃圾股
       .filter(o => o.indicators.count(_.enough) >= 3) // 统计区间不足的
-      .filter(_.avgDownOscillation <= 40) // 低谷驻留时间不超过**
-      .filter(_.avgAmplitude >= 80) // 平均振幅
+      .filter(_.avgDownOscillation <= 42) // 低谷驻留时间不超过**
+      .filter(_.avgAmplitude >= 100) // 平均振幅
+//      .filter(o => o.indicators.head.avg >= o.indicators.head.last * 1.08) // 盈利空间
 
-    r.map(_.toString).foreach(log.info)
+//    r.map(_.toString).foreach(log.info)
 
-    log.info("满足条件的有:{}只", r.size)
-    log.info(r.map(_.stock.code.substring(2)).sorted.mkString(";"))
+    log.info("满足条件的有:{}只\n{}", r.size, r.map(_.stock.code.substring(2)).sorted.mkString(";"))
+
+    r.map(_.stock.code.substring(2)).sorted
   }
 
-  def show(codes: Seq[String]): Unit = {
+  def show(codes: Seq[String], detail: Boolean = false): Unit = {
     val currentMap = SinaQuotesApi.getData(codes).map(o => (o.code.substring(2), o)).toMap
     codes.foreach(showList)
 
@@ -58,16 +72,23 @@ object Application extends App {
       val current = currentMap(r.stock.code)
       val avg = intToDecimal(r.indicators.head.avg)
 
-      log.info(
-        "{}\t{}\t{}\t现价:{}\t均值:{}\t需要{}天\t均值曲线:{}",
-        if (avg > current.currentPrice) "+" else "",
-        code,
-        current.name,
-        current.currentPrice,
-        avg,
-        r.indicators.head.maxDownOscillation,
-        r.indicators.map(_.avg).map(intToDecimal).mkString("[", ", ", "]")
-      )
+      if (detail) {
+        log.info(r.toString)
+      }
+
+      if (avg >= current.currentPrice * 1.05 && current.currentPrice <= 60) {
+        log.info(
+          "{}\t{}\t{}\t现价:{}\t均值:{}\t需要{}天\t均值曲线:{}\t震荡曲线:{}",
+          if (avg > current.currentPrice) "+" else "",
+          code,
+          current.name,
+          current.currentPrice,
+          avg,
+          r.indicators.head.maxDownOscillation,
+          r.indicators.map(_.avg).map(intToDecimal).mkString("[", ", ", "]"),
+          r.indicators.map(_.maxDownOscillation).mkString("[", ", ", "]")
+        )
+      }
     }
   }
 }
